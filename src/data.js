986301b428a59795b9c0f76c55e236347dd68952
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-13 15:00:39 -0500

    created data.js and imported it to App.js

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const data = [
+  {
+    task: "Watch Precourse Videos",
+    id: "1120",
+    completed: false
+  },
+  {
+    task: "Read More About Topic",
+    id: "1220",
+    completed: false
+  },
+  {
+    task: "Fork Assignment",
+    id: "1320",
+    completed: false
+  },
+  {
+    task: "Clone Assignment",
+    id: "1420",
+    completed: false
+  },
+  {
+    task: "Create Own Branch",
+    id: "1520",
+    completed: false
+  },
+  {
+    task: "Read Instructions Carefully",
+    id: "1620",
+    completed: false
+  },
+  {
+    task: "Plan Your Course of Action",
+    id: "1720",
+    completed: false
+  },
+  {
+    task: "Start Coding",
+    id: "1820",
+    completed: false
+  },
+  {
+    task: "Debug",
+    id: "1920",
+    completed: false
+  }
+];
