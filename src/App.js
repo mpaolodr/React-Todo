edb037b78c7130410031245f3d31491d3d59c2e9
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-13 15:13:40 -0500

    created togglecompleted function and will be passed as props to TodoList

--- src/App.js
+++ src/App.js
@@ -11,12 +11,29 @@ class App extends React.Component {
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
 
+  //constructor function for my state
   constructor() {
     super();
     this.state = {
       todoList: data
     };
   }
+
+  //function to toggle if task is completed
+  toggleCompleted = itemId => {
+    this.setState({
+      todoList: this.state.todoList.map(item => {
+        if (item.id === itemId) {
+          return {
+            ...item,
+            completed: !item.completed
+          };
+        } else {
+          return item;
+        }
+      })
+    });
+  };
 
   render() {
     return (
